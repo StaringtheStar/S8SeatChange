e645339589d682b20d845cfcaf59d84ba1d43a7e
StaringtheStar <staringthestar@gmail.com>
2019-03-05 21:12:08 +0900

    ScreenCapture Included

--- S8SeatChange/FrmMain.cs
+++ S8SeatChange/FrmMain.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,66 @@ namespace S8SeatChange
         public FrmMain()
         {
             InitializeComponent();
+            this.Icon = S8SeatChange.Properties.Resources.S8;
+        }
+
+        //From https://stackoverflow.com/a/24879511
+        //By Christian Moser
+
+        public class ScreenCapture
+        {
+            [DllImport("user32.dll")]
+            private static extern IntPtr GetForegroundWindow();
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+            public static extern IntPtr GetDesktopWindow();
+
+            [StructLayout(LayoutKind.Sequential)]
+            private struct Rect
+            {
+                public int Left;
+                public int Top;
+                public int Right;
+                public int Bottom;
+            }
+
+            [DllImport("user32.dll")]
+            private static extern IntPtr GetWindowRect(IntPtr hWnd, ref Rect rect);
+
+            public static Image CaptureDesktop()
+            {
+                return CaptureWindow(GetDesktopWindow());
+            }
+
+            public static Bitmap CaptureActiveWindow()
+            {
+                return CaptureWindow(GetForegroundWindow());
+            }
+
+            public static Bitmap CaptureWindow(IntPtr handle)
+            {
+                var rect = new Rect();
+                GetWindowRect(handle, ref rect);
+                var bounds = new Rectangle(rect.Left, rect.Top, rect.Right - rect.Left, rect.Bottom - rect.Top);
+                var result = new Bitmap(bounds.Width, bounds.Height);
+
+                using (var graphics = Graphics.FromImage(result))
+                {
+                    graphics.CopyFromScreen(new Point(bounds.Left, bounds.Top), Point.Empty, bounds.Size);
+                }
+
+                return result;
+            }
+        }
+
+        //use as below:
+        //var image=ScreenCapture.CaptureActiveWindow();
+        //image.Save(@"C:\temp\~~.jpg", ImageFormat.Jpeg);
+
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            
         }
     }
 }
